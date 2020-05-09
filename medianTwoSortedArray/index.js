fd54f199601aa964a16bf707d17491efb34d9fd4
Rahmat Hidayat <bebensiganteng@gmail.com>
2020-05-09 17:55:45 +0900

    Update index.js

--- medianTwoSortedArray/index.js
+++ medianTwoSortedArray/index.js
@@ -3,7 +3,7 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays1 = function(nums1, nums2) {
+var findMedianSortedArrays = function(nums1, nums2) {
 
   const all = nums1.concat(nums2).sort((a,b) => {
     return a - b;
