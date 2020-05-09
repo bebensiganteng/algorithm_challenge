bbca58767ebc83e8df0948e52fd9ed4e8dd2bbe3
Rahmat Hidayat <rahmat.hidayat@tbwahakuhodo.co.jp>
2020-05-09 20:56:54 +0900

    brute force

--- medianTwoSortedArray/index.js
+++ medianTwoSortedArray/index.js
@@ -1,9 +1,10 @@
 /**
+ * BRUTE FORCE
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays1 = function(nums1, nums2) {
+var findMedianSortedArrays = function(nums1, nums2) {
 
   const all = nums1.concat(nums2).sort((a,b) => {
     return a - b;
@@ -21,13 +22,14 @@ var findMedianSortedArrays1 = function(nums1, nums2) {
 };
 
 
+
 // const input1 = [1, 3];
 // const input2 = [2];
-// const input1 = [1, 2];
-// const input2 = [3, 4];
+const input1 = [1, 2];
+const input2 = [3, 4];
 // const input1 = [1];
 // const input2 = [4];
-const input1 = [1, 2, 4 ,5];
-const input2 = [6, 3, 10, 11];
+// const input1 = [1, 2, 4 ,5];
+// const input2 = [6, 10, 11];
 const r = findMedianSortedArrays(input1, input2);
 console.log(r);
